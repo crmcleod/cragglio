82e79c336c7bceae243a9019b93e61b01aa4a4f6
Craig McLeod <mcleod.craig@outlook.com>
2024-01-14 17:13:01 +0000

    Update script and minor UI changes

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,7 +1,8 @@
 export const Menu = () => {
     return(
-        <div id='menu' className='w100 h100 flex-column'>
+        <div id='menu' className='w100 flex-column'>
             <img src="https://fontmeme.com/permalink/240111/f45262581f21279817d10f126ed68d31.png" alt="pixel-fonts" border="0"/>
+            <p>A Text Adventure</p>
         </div>
     )
 }
