a5f9d072da2805c67004f42f888e60173f437935
Craig McLeod <mcleod.craig@outlook.com>
2024-02-12 17:06:14 +0000

    Interim commit with part mine game, dev app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './Styles/App.css';
 import { useState, useEffect, createContext } from 'react';
 import axios from 'axios'
 import { Game } from './Game';
+import mineText from './mine.json'
 
 export const MainContext = createContext();
 
@@ -24,7 +25,8 @@ export const App = () => {
 
 
   useEffect(() => {
-    axios.get('https://raw.githubusercontent.com/crmcleod/cragglio/main/src/text.json').then(res => setText(res.data))
+    setText(mineText)
+    // axios.get('https://raw.githubusercontent.com/crmcleod/cragglio/main/src/text.json').then(res => setText(res.data))
     setTimeout(() => { // credit screen
       setIntro(false)
       setTimeout(() => { // title screen
@@ -42,6 +44,12 @@ export const App = () => {
     if (inGame) {
       // if text successfully set to state
       if (text?.[`${storyLevel}`]?.[currentChapterLevel]?.text) {
+
+        ////////////////////////////
+        // TO DO - HANDLE ENDING! //
+        ////////////////////////////
+
+
         setTimeout(() => { // 1.5s time out to allow fade in of text
 
           setCurrentText(text?.[`${storyLevel}`]?.[currentChapterLevel]?.text)
@@ -50,8 +58,8 @@ export const App = () => {
             setCurrentChapterLevel(currentChapterLevel + 1)
             setHidden(true)
 
-          }, (text?.[`${storyLevel}`]?.[currentChapterLevel]?.text?.split(' ').length * 330) + 2000)
-        }, 1500)
+          }, (text?.[`${storyLevel}`]?.[currentChapterLevel]?.text?.split(' ').length * 3) + 20) // 330
+        }, 150) // 1500
       }
       else
         // if text not set and options exist then set options
@@ -88,7 +96,7 @@ export const App = () => {
           advanceStory(tempOptions?.options?.[e]['chapter-level'], tempOptions?.options?.[e]['story-level']) :
           setCurrentChapterLevel(currentChapterLevel + 1)
         setHidden(true)
-      }, (((((tempOptions?.['continuity-text'] ? tempOptions?.['continuity-text'] : ' ') + (tempOptions?.options?.[e]?.action)).split(' ')).length) * 330) + 2000)
+      }, (((((tempOptions?.['continuity-text'] ? tempOptions?.['continuity-text'] : ' ') + (tempOptions?.options?.[e]?.action)).split(' ')).length) * 3) + 2000) // 330
 
     }, 1000)
   }
