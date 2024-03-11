014b26f1a1a568bcb6c5a0d8fd280b5edafaf828
Craig McLeod <mcleod.craig@outlook.com>
2024-03-11 22:09:31 +0000

    Skip text block functionality

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import Dust from "./Dust"
 import DiffusingPixels from "./IntroDiffuse"
 import { Credits } from "./Credits"
@@ -7,10 +7,20 @@ import { OptionsContainer } from "./OptionsContainer"
 import { MainContext } from "./App"
 export const Game = () => {
 
+  const [clicked, setClicked] = useState(false)
+
   const {
-    currentText, currentOptions, storyLevel, currentChapterLevel, hidden, inGame, intro, handleOptionClick, closedRoutes
+    currentText, currentOptions, storyLevel, currentChapterLevel, hidden, inGame, intro, handleOptionClick, closedRoutes,timer, count, setTimer, setCount
   } = useContext(MainContext)
 
+
+  const skipText = () => {
+    setTimer(count + 10)
+    setClicked(true)
+    setTimeout(() => {
+      setClicked(false)
+    },3000)
+  }
   return (
     <>
       <Dust />
@@ -24,6 +34,9 @@ export const Game = () => {
           !intro && !inGame &&
           <Menu />
         }
+          {
+            (timer > 30) && !clicked && inGame && currentText && !currentOptions && <button className={`skip-button ${hidden ? 'hidden' : 'visible'}`} onClick={skipText}>Skip</button>
+          }
         {
           inGame && currentText &&
           <p key={currentChapterLevel + storyLevel + currentOptions?.length} className={`normal-text ${hidden ? 'hidden' : 'visible'}`}>
